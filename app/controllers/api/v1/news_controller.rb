814b7b17f1adff925cfcee25ef294cbf4f190de0
iewad <lidawei.me@gmail.com>
2018-08-21 22:49:37 +0800

    新闻接口进行分类

--- app/controllers/api/v1/news_controller.rb
+++ app/controllers/api/v1/news_controller.rb
@@ -1,6 +1,13 @@
 class Api::V1::NewsController < Api::BaseController
   def index
-    @news = News.select("id, title, summary, addtime, news_id").order(id: :desc).page(params['page']).per(params['per'])
+    @news = News.all.select("id, title, summary, addtime, news_id")
+
+    if params['category'] == 'general'
+      @news = @news.general
+    elsif params['category'] == 'support_notice'
+      @news = @news.support_notice
+    end
+    @news = @news.page(params['page']).per(params['per'])
   end
 
   def show
